caacdf65a706b24d32b4f0c5e528cb075a45c65a
Andrey Sheredega <sheredega303@gmail.com>
2023-01-30 17:10:42 +0200

    Stock view page, Turbo frames & Turbo flash

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,20 +1,17 @@
 class StocksController < ApplicationController
+
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-        # render 'users/my_portfolio'
-        respond_to do |format|
-          format.js {render partial: 'users/result'}
-        end
-      else
-        flash[:alert] = "Please enter a valid symbol to search"
-        redirect_to my_portfolio_path
-      end
 
+      else
+        flash.now[:alert] = "Please enter a valid symbol to search"
+        render turbo_stream: turbo_stream.update("flash", partial: "layouts/messages")
+      end
     else
-      flash[:alert] = "Please enter a symbol to search"
-      redirect_to my_portfolio_path
+      flash.now[:alert] = "Please enter a symbol to search"
+      render turbo_stream: turbo_stream.update("flash", partial: "layouts/messages")
     end
   end
 end
